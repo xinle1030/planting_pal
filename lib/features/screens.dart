38ca045de58465733f8c3bd4326a5396cc0479b9
xinle1030 <xinlelam1030@gmail.com>
2023-03-16 06:30:02 +0800

    add camera page and preview page

--- lib/features/screens.dart
+++ lib/features/screens.dart
@@ -1,3 +1,6 @@
 export 'dashboard/screens/in_progress_dashboard.dart';
 export 'dashboard/screens/unfulfilled_dashboard.dart';
 export 'dashboard/screens/planting_dashboard.dart';
+export 'camera/camera_page.dart';
+export 'camera/preview_page.dart';
+
