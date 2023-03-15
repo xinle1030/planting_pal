7e00eceddbbc75aff037c150a90c5292cd0a84df
xinle1030 <xinlelam1030@gmail.com>
2023-03-15 17:02:15 +0800

    update responsiveness

--- lib/config/responsive.dart
+++ lib/config/responsive.dart
@@ -1,48 +1,36 @@
 import 'package:flutter/material.dart';
 
 class Responsive extends StatelessWidget {
-  final Widget? mobile;
+  final Widget mobile;
   final Widget? tablet;
-  final Widget? desktop;
-  final Widget? smallMobile;
+  final Widget desktop;
 
   const Responsive({
     Key? key,
     required this.mobile,
     this.tablet,
     required this.desktop,
-    this.smallMobile
   }) : super(key: key);
 
-// This size work fine on my design, maybe you need some customization depends on your design
-
-  // This isMobile, isTablet, isDesktop help us later
   static bool isMobile(BuildContext context) =>
-      MediaQuery.of(context).size.width < 768;
+      MediaQuery.of(context).size.width < 576;
 
   static bool isTablet(BuildContext context) =>
-      MediaQuery.of(context).size.width < 1200 &&
-      MediaQuery.of(context).size.width >= 768;
+      MediaQuery.of(context).size.width >= 576 &&
+      MediaQuery.of(context).size.width <= 992;
 
   static bool isDesktop(BuildContext context) =>
-      MediaQuery.of(context).size.width >= 1200;
+      MediaQuery.of(context).size.width > 992;
 
   @override
   Widget build(BuildContext context) {
-    final Size _size = MediaQuery.of(context).size;
-    // If our width is more than 1200 then we consider it a desktop
-    if (_size.width >= 1200) {
-      return desktop!;
-    }
-    // If width it less then 1200 and more then 768 we consider it as tablet
-    else if (_size.width >= 768 && tablet != null) {
+    final Size size = MediaQuery.of(context).size;
+    if (size.width > 992) {
+      return desktop;
+    } else if (size.width >= 576 && tablet != null) {
       return tablet!;
-    }
-    // Or less then that we called it mobile
-    else if (_size.width >= 376 && _size.width <= 768 && mobile != null) {
-      return mobile!;
     } else {
-      return smallMobile!;
+      return mobile;
     }
   }
 }
