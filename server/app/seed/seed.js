a87d85e1dc3184ec9b1a9f3d4d1d987be4e820c1
ChawKokFei <80729400+ChawKokFei@users.noreply.github.com>
2023-03-16 09:42:29 +0800

    Update seed file

--- server/app/seed/seed.js
+++ server/app/seed/seed.js
@@ -20,112 +20,455 @@ exports.initial = () => {
   });
 
   User.create({
-    username: "user1",
-    email: "user1@mail.com",
-    password: "user1",
+    username: "Chaw",
+    email: "apedell88@gmail.com",
+    password: "Chaw",
   });
 
   User.create({
-    username: "user2",
-    email: "user2@mail.com",
-    password: "user2",
+    username: "Kok",
+    email: "apedell88@gmail.com",
+    password: "Kok",
   });
 
   User.create({
-    username: "user3",
-    email: "user3@mail.com",
-    password: "user3",
+    username: "Fei",
+    email: "apedell88@gmail.com",
+    password: "Fei",
   });
 
   Order.create({
-    orderDate: "2021-01-01",
+    orderDate: "2023-01-01",
     userId: 1,
-    nameOfBuyer: "user1",
-    receiverName: "user1",
-    receiverEmail: "chawkokfei@gmail.com",
+    nameOfBuyer: "Chaw",
+    receiverName: "Kok",
+    receiverEmail: "apedell88@gmail.com",
     greetings: "Happy Birthday!",
-    treeCoordinatesRequired: true,
-    treeCoordinates: "test",
     numberOfTrees: 1,
     amountReceived: 1.0,
     countryOfOrigin: "Malaysia",
-    orderStatus: "Unfulfilled",
+  });
+
+  Order.create({
+    orderDate: "2023-01-02",
+    userId: 2,
+    nameOfBuyer: "Kok",
+    receiverName: "Fei",
+    receiverEmail: "apedell88@gmail.com",
+    greetings: "Happy Birthday 2!",
+    numberOfTrees: 2,
+    amountReceived: 1.2,
+    countryOfOrigin: "Malaysia",
+  });
+
+  Order.create({
+    orderDate: "2023-01-03",
+    userId: 1,
+    nameOfBuyer: "Chaw",
+    receiverName: "Kok",
+    receiverEmail: "apedell88@gmail.com",
+    greetings: "Happy Birthday 3!",
+    numberOfTrees: 3,
+    amountReceived: 1.4,
+    countryOfOrigin: "Malaysia",
+  });
+
+  Order.create({
+    orderDate: "2023-01-04",
+    userId: 1,
+    nameOfBuyer: "Chaw",
+    receiverName: "Kok",
+    receiverEmail: "apedell88@gmail.com",
+    greetings: "Happy Birthday 3!",
+    numberOfTrees: 3,
+    amountReceived: 1.4,
+    countryOfOrigin: "Malaysia",
+  });
+
+  Order.create({
+    orderDate: "2023-01-05",
+    userId: 1,
+    nameOfBuyer: "Chaw",
+    receiverName: "Kok",
+    receiverEmail: "apedell88@gmail.com",
+    greetings: "Happy Birthday 3!",
+    numberOfTrees: 3,
+    amountReceived: 1.4,
+    countryOfOrigin: "Malaysia",
   });
 
   Order.create({
     orderDate: "2021-01-01",
     userId: 2,
-    nameOfBuyer: "user2",
-    receiverName: "user2",
-    receiverEmail: "chawkokfei@gmail.com",
+    nameOfBuyer: "Chaw",
+    receiverName: "Kok",
+    receiverEmail: "apedell88@gmail.com",
     greetings: "Happy Birthday!",
     treeCoordinatesRequired: true,
-    treeCoordinates: "test",
+    treeCoordinates: "12.231, 14.254",
     numberOfTrees: 1,
     amountReceived: 1.2,
     countryOfOrigin: "Malaysia",
-    orderStatus: "Partially Fulfilled",
     orderStatus: "In Progress",
     pdfLink:
-      "https://th.bing.com/th/id/OIP.CBFZpMOFqyCjyHOJxouwVAHaE8?pid=ImgDet&rs=1",
+      "https://drive.google.com/uc?id=1ehS1s1fUN42waTF5xKOetJOreWQf7rb2&export=download",
+  });
+
+  Order.create({
+    orderDate: "2021-01-01",
+    userId: 2,
+    nameOfBuyer: "Chaw",
+    receiverName: "Kok",
+    receiverEmail: "apedell88@gmail.com",
+    greetings: "Happy Birthday!",
+    treeCoordinatesRequired: true,
+    treeCoordinates: "12.231, 14.254",
+    numberOfTrees: 1,
+    amountReceived: 1.2,
+    countryOfOrigin: "Malaysia",
+    orderStatus: "In Progress",
+    pdfLink:
+      "https://drive.google.com/uc?id=1ehS1s1fUN42waTF5xKOetJOreWQf7rb2&export=download",
+  });
+
+  Order.create({
+    orderDate: "2021-01-01",
+    userId: 2,
+    nameOfBuyer: "Chaw",
+    receiverName: "Kok",
+    receiverEmail: "apedell88@gmail.com",
+    greetings: "Happy Birthday!",
+    treeCoordinatesRequired: true,
+    treeCoordinates: "12.231, 14.254",
+    numberOfTrees: 1,
+    amountReceived: 1.2,
+    countryOfOrigin: "Malaysia",
+    orderStatus: "In Progress",
+    pdfLink:
+      "https://drive.google.com/uc?id=1ehS1s1fUN42waTF5xKOetJOreWQf7rb2&export=download",
+  });
+
+  Order.create({
+    orderDate: "2021-01-01",
+    userId: 2,
+    nameOfBuyer: "Chaw",
+    receiverName: "Kok",
+    receiverEmail: "apedell88@gmail.com",
+    greetings: "Happy Birthday!",
+    treeCoordinatesRequired: true,
+    treeCoordinates: "12.231, 14.254",
+    numberOfTrees: 1,
+    amountReceived: 1.2,
+    countryOfOrigin: "Malaysia",
+    orderStatus: "In Progress",
+    pdfLink:
+      "https://drive.google.com/uc?id=1ehS1s1fUN42waTF5xKOetJOreWQf7rb2&export=download",
+  });
+
+  Order.create({
+    orderDate: "2021-01-01",
+    userId: 2,
+    nameOfBuyer: "Chaw",
+    receiverName: "Kok",
+    receiverEmail: "apedell88@gmail.com",
+    greetings: "Happy Birthday!",
+    treeCoordinatesRequired: true,
+    treeCoordinates: "12.231, 14.254",
+    numberOfTrees: 1,
+    amountReceived: 1.2,
+    countryOfOrigin: "Malaysia",
+    orderStatus: "In Progress",
+    pdfLink:
+      "https://drive.google.com/uc?id=1ehS1s1fUN42waTF5xKOetJOreWQf7rb2&export=download",
   });
 
   Order.create({
     orderDate: "2021-01-01",
     userId: 3,
-    nameOfBuyer: "user3",
-    receiverName: "user3",
-    receiverEmail: "chawkokfei@gmail.com",
+    nameOfBuyer: "Kok",
+    receiverName: "Fei",
+    receiverEmail: "apedell88@gmail.com",
     greetings: "Happy Birthday!",
     treeCoordinatesRequired: true,
-    treeCoordinates: "test",
+    treeCoordinates: "12.231, 14.254",
     numberOfTrees: 1,
     amountReceived: 11.0,
     countryOfOrigin: "Malaysia",
     orderStatus: "Partially Fulfilled",
     pdfLink:
-      "https://th.bing.com/th/id/OIP.CBFZpMOFqyCjyHOJxouwVAHaE8?pid=ImgDet&rs=1",
-    photoLink:
-      "12fd20616246da3ee5d42347bea24c5340ba77afd772213ec60ddead95030bb2.jpg",
+      "https://drive.google.com/uc?id=1ehS1s1fUN42waTF5xKOetJOreWQf7rb2&export=download",
+    photoLink: "https://tinyurl.com/2dweypw6",
   });
 
   Order.create({
     orderDate: "2021-01-01",
     userId: 3,
-    nameOfBuyer: "user4",
-    receiverName: "user4",
-    receiverEmail: "chawkokfei@gmail.com",
+    nameOfBuyer: "Kok",
+    receiverName: "Fei",
+    receiverEmail: "apedell88@gmail.com",
     greetings: "Happy Birthday!",
     treeCoordinatesRequired: true,
-    treeCoordinates: "test",
+    treeCoordinates: "12.231, 14.254",
     numberOfTrees: 1,
-    amountReceived: 1.0,
+    amountReceived: 11.0,
     countryOfOrigin: "Malaysia",
-    orderStatus: "Almost fulfilled",
+    orderStatus: "Partially Fulfilled",
     pdfLink:
-      "https://th.bing.com/th/id/OIP.CBFZpMOFqyCjyHOJxouwVAHaE8?pid=ImgDet&rs=1",
-    photoLink:
-      "12fd20616246da3ee5d42347bea24c5340ba77afd772213ec60ddead95030bb2.jpg",
+      "https://drive.google.com/uc?id=1ehS1s1fUN42waTF5xKOetJOreWQf7rb2&export=download",
+    photoLink: "https://tinyurl.com/2dweypw6",
   });
 
   Order.create({
     orderDate: "2021-01-01",
     userId: 3,
-    nameOfBuyer: "user5",
-    receiverName: "user5",
-    receiverEmail: "chawkokfei@gmail.com",
+    nameOfBuyer: "Kok",
+    receiverName: "Fei",
+    receiverEmail: "apedell88@gmail.com",
     greetings: "Happy Birthday!",
     treeCoordinatesRequired: true,
-    treeCoordinates: "test",
+    treeCoordinates: "12.231, 14.254",
     numberOfTrees: 1,
-    amountReceived: 1.0,
+    amountReceived: 11.0,
+    countryOfOrigin: "Malaysia",
+    orderStatus: "Partially Fulfilled",
+    pdfLink:
+      "https://drive.google.com/uc?id=1ehS1s1fUN42waTF5xKOetJOreWQf7rb2&export=download",
+    photoLink: "https://tinyurl.com/2dweypw6",
+  });
+
+  Order.create({
+    orderDate: "2021-01-01",
+    userId: 3,
+    nameOfBuyer: "Kok",
+    receiverName: "Fei",
+    receiverEmail: "apedell88@gmail.com",
+    greetings: "Happy Birthday!",
+    treeCoordinatesRequired: true,
+    treeCoordinates: "12.231, 14.254",
+    numberOfTrees: 1,
+    amountReceived: 11.0,
+    countryOfOrigin: "Malaysia",
+    orderStatus: "Partially Fulfilled",
+    pdfLink:
+      "https://drive.google.com/uc?id=1ehS1s1fUN42waTF5xKOetJOreWQf7rb2&export=download",
+    photoLink: "https://tinyurl.com/2dweypw6",
+  });
+
+  Order.create({
+    orderDate: "2021-01-01",
+    userId: 3,
+    nameOfBuyer: "Kok",
+    receiverName: "Fei",
+    receiverEmail: "apedell88@gmail.com",
+    greetings: "Happy Birthday!",
+    treeCoordinatesRequired: true,
+    treeCoordinates: "12.231, 14.254",
+    numberOfTrees: 1,
+    amountReceived: 11.0,
+    countryOfOrigin: "Malaysia",
+    orderStatus: "Partially Fulfilled",
+    pdfLink:
+      "https://drive.google.com/uc?id=1ehS1s1fUN42waTF5xKOetJOreWQf7rb2&export=download",
+    photoLink: "https://tinyurl.com/2dweypw6",
+  });
+
+  Order.create({
+    orderDate: "2021-01-01",
+    userId: 3,
+    nameOfBuyer: "Kok",
+    receiverName: "Fei",
+    receiverEmail: "apedell88@gmail.com",
+    greetings: "Happy Birthday!",
+    treeCoordinatesRequired: true,
+    treeCoordinates: "12.231, 14.254",
+    numberOfTrees: 1,
+    amountReceived: 11.0,
+    countryOfOrigin: "Malaysia",
+    orderStatus: "Almost Fulfilled",
+    pdfLink:
+      "https://drive.google.com/uc?id=1ehS1s1fUN42waTF5xKOetJOreWQf7rb2&export=download",
+    photoLink: "https://tinyurl.com/2dweypw6",
+    certLink:
+      "https://drive.google.com/uc?id=1sbTWuCnDNKkxE_t0SDuYfxoUH9Cjtkz-&export=download",
+  });
+
+  Order.create({
+    orderDate: "2021-01-01",
+    userId: 3,
+    nameOfBuyer: "Kok",
+    receiverName: "Fei",
+    receiverEmail: "apedell88@gmail.com",
+    greetings: "Happy Birthday!",
+    treeCoordinatesRequired: true,
+    treeCoordinates: "12.231, 14.254",
+    numberOfTrees: 1,
+    amountReceived: 11.0,
+    countryOfOrigin: "Malaysia",
+    orderStatus: "Almost Fulfilled",
+    pdfLink:
+      "https://drive.google.com/uc?id=1ehS1s1fUN42waTF5xKOetJOreWQf7rb2&export=download",
+    photoLink: "https://tinyurl.com/2dweypw6",
+    certLink:
+      "https://drive.google.com/uc?id=1sbTWuCnDNKkxE_t0SDuYfxoUH9Cjtkz-&export=download",
+  });
+
+  Order.create({
+    orderDate: "2021-01-01",
+    userId: 3,
+    nameOfBuyer: "Kok",
+    receiverName: "Fei",
+    receiverEmail: "apedell88@gmail.com",
+    greetings: "Happy Birthday!",
+    treeCoordinatesRequired: true,
+    treeCoordinates: "12.231, 14.254",
+    numberOfTrees: 1,
+    amountReceived: 11.0,
+    countryOfOrigin: "Malaysia",
+    orderStatus: "Almost Fulfilled",
+    pdfLink:
+      "https://drive.google.com/uc?id=1ehS1s1fUN42waTF5xKOetJOreWQf7rb2&export=download",
+    photoLink: "https://tinyurl.com/2dweypw6",
+    certLink:
+      "https://drive.google.com/uc?id=1sbTWuCnDNKkxE_t0SDuYfxoUH9Cjtkz-&export=download",
+  });
+
+  Order.create({
+    orderDate: "2021-01-01",
+    userId: 3,
+    nameOfBuyer: "Kok",
+    receiverName: "Fei",
+    receiverEmail: "apedell88@gmail.com",
+    greetings: "Happy Birthday!",
+    treeCoordinatesRequired: true,
+    treeCoordinates: "12.231, 14.254",
+    numberOfTrees: 1,
+    amountReceived: 11.0,
+    countryOfOrigin: "Malaysia",
+    orderStatus: "Almost Fulfilled",
+    pdfLink:
+      "https://drive.google.com/uc?id=1ehS1s1fUN42waTF5xKOetJOreWQf7rb2&export=download",
+    photoLink: "https://tinyurl.com/2dweypw6",
+    certLink:
+      "https://drive.google.com/uc?id=1sbTWuCnDNKkxE_t0SDuYfxoUH9Cjtkz-&export=download",
+  });
+
+  Order.create({
+    orderDate: "2021-01-01",
+    userId: 3,
+    nameOfBuyer: "Kok",
+    receiverName: "Fei",
+    receiverEmail: "apedell88@gmail.com",
+    greetings: "Happy Birthday!",
+    treeCoordinatesRequired: true,
+    treeCoordinates: "12.231, 14.254",
+    numberOfTrees: 1,
+    amountReceived: 11.0,
+    countryOfOrigin: "Malaysia",
+    orderStatus: "Almost Fulfilled",
+    pdfLink:
+      "https://drive.google.com/uc?id=1ehS1s1fUN42waTF5xKOetJOreWQf7rb2&export=download",
+    photoLink: "https://tinyurl.com/2dweypw6",
+    certLink:
+      "https://drive.google.com/uc?id=1sbTWuCnDNKkxE_t0SDuYfxoUH9Cjtkz-&export=download",
+  });
+
+  Order.create({
+    orderDate: "2021-01-01",
+    userId: 3,
+    nameOfBuyer: "Kok",
+    receiverName: "Fei",
+    receiverEmail: "apedell88@gmail.com",
+    greetings: "Happy Birthday!",
+    treeCoordinatesRequired: true,
+    treeCoordinates: "12.231, 14.254",
+    numberOfTrees: 1,
+    amountReceived: 11.0,
     countryOfOrigin: "Malaysia",
     orderStatus: "Fulfilled",
     pdfLink:
-      "https://th.bing.com/th/id/OIP.CBFZpMOFqyCjyHOJxouwVAHaE8?pid=ImgDet&rs=1",
+      "https://drive.google.com/uc?id=1ehS1s1fUN42waTF5xKOetJOreWQf7rb2&export=download",
+    photoLink: "https://tinyurl.com/2dweypw6",
     certLink:
-      "https://pulpbits.net/wp-content/uploads/2014/01/Gray-Tabby-Cat1.jpg",
-    photoLink:
-      "12fd20616246da3ee5d42347bea24c5340ba77afd772213ec60ddead95030bb2.jpg",
+      "https://drive.google.com/uc?id=1sbTWuCnDNKkxE_t0SDuYfxoUH9Cjtkz-&export=download",
+  });
+
+  Order.create({
+    orderDate: "2021-01-01",
+    userId: 3,
+    nameOfBuyer: "Kok",
+    receiverName: "Fei",
+    receiverEmail: "apedell88@gmail.com",
+    greetings: "Happy Birthday!",
+    treeCoordinatesRequired: true,
+    treeCoordinates: "12.231, 14.254",
+    numberOfTrees: 1,
+    amountReceived: 11.0,
+    countryOfOrigin: "Malaysia",
+    orderStatus: "Fulfilled",
+    pdfLink:
+      "https://drive.google.com/uc?id=1ehS1s1fUN42waTF5xKOetJOreWQf7rb2&export=download",
+    photoLink: "https://tinyurl.com/2dweypw6",
+    certLink:
+      "https://drive.google.com/uc?id=1sbTWuCnDNKkxE_t0SDuYfxoUH9Cjtkz-&export=download",
+  });
+
+  Order.create({
+    orderDate: "2021-01-01",
+    userId: 3,
+    nameOfBuyer: "Kok",
+    receiverName: "Fei",
+    receiverEmail: "apedell88@gmail.com",
+    greetings: "Happy Birthday!",
+    treeCoordinatesRequired: true,
+    treeCoordinates: "12.231, 14.254",
+    numberOfTrees: 1,
+    amountReceived: 11.0,
+    countryOfOrigin: "Malaysia",
+    orderStatus: "Fulfilled",
+    pdfLink:
+      "https://drive.google.com/uc?id=1ehS1s1fUN42waTF5xKOetJOreWQf7rb2&export=download",
+    photoLink: "https://tinyurl.com/2dweypw6",
+    certLink:
+      "https://drive.google.com/uc?id=1sbTWuCnDNKkxE_t0SDuYfxoUH9Cjtkz-&export=download",
+  });
+
+  Order.create({
+    orderDate: "2021-01-01",
+    userId: 3,
+    nameOfBuyer: "Kok",
+    receiverName: "Fei",
+    receiverEmail: "apedell88@gmail.com",
+    greetings: "Happy Birthday!",
+    treeCoordinatesRequired: true,
+    treeCoordinates: "12.231, 14.254",
+    numberOfTrees: 1,
+    amountReceived: 11.0,
+    countryOfOrigin: "Malaysia",
+    orderStatus: "Fulfilled",
+    pdfLink:
+      "https://drive.google.com/uc?id=1ehS1s1fUN42waTF5xKOetJOreWQf7rb2&export=download",
+    photoLink: "https://tinyurl.com/2dweypw6",
+    certLink:
+      "https://drive.google.com/uc?id=1sbTWuCnDNKkxE_t0SDuYfxoUH9Cjtkz-&export=download",
+  });
+
+  Order.create({
+    orderDate: "2021-01-01",
+    userId: 3,
+    nameOfBuyer: "Kok",
+    receiverName: "Fei",
+    receiverEmail: "apedell88@gmail.com",
+    greetings: "Happy Birthday!",
+    treeCoordinatesRequired: true,
+    treeCoordinates: "12.231, 14.254",
+    numberOfTrees: 1,
+    amountReceived: 11.0,
+    countryOfOrigin: "Malaysia",
+    orderStatus: "Fulfilled",
+    pdfLink:
+      "https://drive.google.com/uc?id=1ehS1s1fUN42waTF5xKOetJOreWQf7rb2&export=download",
+    photoLink: "https://tinyurl.com/2dweypw6",
+    certLink:
+      "https://drive.google.com/uc?id=1sbTWuCnDNKkxE_t0SDuYfxoUH9Cjtkz-&export=download",
   });
 };
